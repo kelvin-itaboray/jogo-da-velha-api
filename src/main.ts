ac38a2b7b855d27109167e8fb3a318d1dab08772
Kelvin <kelvin@formahomolog.com.br>
2021-10-23 00:52:39 -0300

    fix: adicionando porta em env e Procfile

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
+import * as dotenv from 'dotenv';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+dotenv.config();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
