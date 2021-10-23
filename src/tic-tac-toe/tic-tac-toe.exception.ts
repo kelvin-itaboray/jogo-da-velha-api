99b27c3df42ef8a6ca971a0d0e1819c46a1ecfc8
Kelvin <kelvin@formahomolog.com.br>
2021-10-22 22:25:12 -0300

    feat: nova exceção GAME_ALREADY_FINISHED

--- src/tic-tac-toe/tic-tac-toe.exception.ts
+++ src/tic-tac-toe/tic-tac-toe.exception.ts
@@ -2,4 +2,6 @@ export class TicTacToeException {
   public static readonly INVALID_PLAYER_TURN =
     "It is not player o's turn to play.";
   public static readonly NO_MOVES_LEFT = 'There are no moves left to play.';
+  public static readonly GAME_ALREADY_FINISHED =
+    'No move is possible. Game is already finished.';
 }
