dec30e2fcee66ae20dddb791a63855a3011dae12
Kelvin <kelvin@formahomolog.com.br>
2021-10-22 23:48:50 -0300

    refactoring: atualizando README e exceções

--- src/tic-tac-toe/tic-tac-toe.exception.ts
+++ src/tic-tac-toe/tic-tac-toe.exception.ts
@@ -1,4 +1,9 @@
 export class TicTacToeException {
+  public static readonly INVALID_LENGTH = 'Board must contain 9 characters.';
+  public static readonly INVALID_BOARD =
+    "A query param 'board' must be informed.";
+  public static readonly INVALID_BOARD_VALUE =
+    "Board must contain only 'o', 'x' or empty spaces as characters.";
   public static readonly INVALID_PLAYER_TURN =
     "It is not player o's turn to play.";
   public static readonly NO_MOVES_LEFT = 'There are no moves left to play.';
